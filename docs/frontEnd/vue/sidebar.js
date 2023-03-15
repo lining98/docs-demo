9fc30a49e99ac03d248043e5e93ae600042d3658
lining98 <2357490894@qq.com>
2023-03-15 17:30:37 +0800

    docs:v3

--- docs/frontEnd/vue/sidebar.js
+++ docs/frontEnd/vue/sidebar.js
@@ -5,5 +5,13 @@ module.exports = [
       //   { text: 'vue核心基础', link: '/frontEnd/vue/' },
       //   // { text: 'vue3', link: '/frontEnd/vue3/' },
       // ]
-    }
+    },
+    {
+      text: 'Vue3快速上手',
+      items: [
+        { text: '认识Vue3', link: '/frontEnd/vue/v3/' },
+        { text: '创建Vue3项目', link: '/frontEnd/vue/v3/create' },
+        // { text: 'vue3', link: '/frontEnd/vue3/' },
+      ]
+    },
   ]
---
+++ docs/frontEnd/vue/v3/create.md
@@ -0,0 +1,19 @@
+# 创建Vue3项目
+
+## 1. 使用vue-cli创建
+
+
+## 2. 使用vite创建
+vite的优势
+- 冷服务   默认的构建目标浏览器是能 在 script 标签上支持原生 ESM 和 原生 ESM 动态导入
+- HMR  速度快到惊人的 模块热更新（HMR）
+- Rollup打包  它使用 Rollup 打包你的代码，并且它是预配置的 并且支持大部分rollup插件
+
+使用vite初始化一个项目
+```bash
+# npm
+npm init vite@latest
+
+# yarn
+yarn create vite
+```
